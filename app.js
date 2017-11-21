c1fcebf9afcadbc39afb725fc488864c2c9a7fcd
kaojiaqi <st112343@gmail.com>
2017-11-21 14:46:17 +0800

    1. Fix app listen.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 5000;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -9,6 +8,4 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use("/",express.static(__dirname+"/views"));
 
 
-app.listen(port,() => [
-    console.log("Listen on " + port)
-])
+app.listen(process.env.PORT || 3000);

146365fa6bf2764d4b1085e25fd654d06c092bca
kaojiaqi <st112343@gmail.com>
2017-11-21 15:39:35 +0800

    1. In app.js get cookie

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.get("/",function(req,res) {
+    console.log(req.cookies)
+    res.send("Hello World!")
+})
+
 app.use("/cookies",express.static(__dirname+"/views"));
+
+
 
 app.listen(process.env.PORT || 3000);

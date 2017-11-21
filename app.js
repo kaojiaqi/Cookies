95c942d35c8ffc6ca4b2023835ecb9e2ea75dabc
kaojiaqi <st112343@gmail.com>
2017-11-21 15:01:58 +0800

    1. Test Set Cookies

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use("/",express.static(__dirname+"/views"));
+app.use("/cookies",express.static(__dirname+"/views"));
+
+app.get("/",function(req,res){
+    res.cookie('user','username',{maxAge:10800},send('cookie set'));
+})
 
 
 app.listen(process.env.PORT || 3000);

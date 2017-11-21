c2a83d3fe1a8b718ae4a322aa9d6a6f080f35706
kaojiaqi <st112343@gmail.com>
2017-11-21 15:16:13 +0800

    no message

--- app.js
+++ app.js
@@ -7,9 +7,4 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use("/cookies",express.static(__dirname+"/views"));
 
-app.get("/",function(req,res){
-    res.cookie('user','username',{maxAge:10800},send('cookie set'));
-})
-
-
 app.listen(process.env.PORT || 3000);
